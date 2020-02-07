f11da0e286372aeb3ad8fd0bf3e1158fb3373524
fdango <ccguangyao@gmail.com>
2020-02-07 11:51:03 +0800

    addtest

---
+++ test/unit/lib/add.test.js
@@ -0,0 +1,76 @@
+const {assert, soterBuilder, net } = require('../../helpers/includes')
+const TronWeb = require('tronweb');
+
+const FormData = require('form-data');
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
+
+describe('#Add unit test', function () {
+
+    let soter;
+
+    before(async function () {
+        this.timeout(10000)
+        soter = soterBuilder.createInstance(net)
+    });
+
+    after(async function () {
+        this.timeout(10000)
+    });
+
+    // balance
+    describe('#Add', function () {
+        this.timeout(10000)
+
+        it('addFile', async function () {
+            const newTronWeb = new TronWeb({
+                fullHost: 'https://api.trongrid.io',
+                privateKey: '07843e997dfe76f655274e14ea688ca1ba08550b93147fc0feca4ba7174868a7'
+            });
+     
+            
+        let files = fs.readFileSync(path.join(__dirname, '1.txt'),'utf-8');
+        let addTimestamp = Date.parse(new Date())
+     
+        let addUnSignData = {
+            request_user: newTronWeb.defaultAddress.base58,
+            signed_user: newTronWeb.defaultAddress.base58,
+            request_id: "0b02aa3b-e3a3-489b-9ecf-40607c153b6c",
+            timestamp: addTimestamp
+        }
+
+
+        let addSignature = await newTronWeb.trx.sign(newTronWeb.toHex(JSON.stringify(addUnSignData)))
+
+        let addFormData = new FormData();
+        addFormData.append("raw_data", JSON.stringify(addUnSignData));
+        addFormData.append("file", files);
+        addFormData.append("signature", addSignature)
+        const axiosInstance = axios.create({
+            baseURL: 'http://65.52.163.204:8101/',
+            /* other custom settings */
+          });
+          try{
+            const addSignResponse = axiosInstance({
+                url: `/api/v1/add`,
+                data: addFormData,
+                method: 'post'
+              })
+              console.log(addSignResponse)
+          }catch(err){
+            console.log(err)
+          }
+         
+
+        
+        // let res = await soter.add.addFile(files)
+            // assert.equal(res.code, 0)
+        });
+
+
+
+    });
+
+
+})
--- test/unit/lib/set.test.js
+++ test/unit/lib/set.test.js
@@ -1,53 +1,53 @@
-const {assert, soterBuilder, net } = require('../../helpers/includes')
-const TronWeb = require('tronweb');
+// const {assert, soterBuilder, net } = require('../../helpers/includes')
+// const TronWeb = require('tronweb');
 
-describe('#Set functional unit test', function () {
+// describe('#Set functional unit test', function () {
 
-    let soter;
+//     let soter;
 
-    before(async function () {
-        this.timeout(10000)
-        soter = soterBuilder.createInstance(net)
-    });
+//     before(async function () {
+//         this.timeout(10000)
+//         soter = soterBuilder.createInstance(net)
+//     });
 
-    after(async function () {
-        this.timeout(10000)
-    });
+//     after(async function () {
+//         this.timeout(10000)
+//     });
 
-    //  SetAutopay
-    describe('#Set SetAutopay', function () {
-        this.timeout(10000)
+//     //  SetAutopay
+//     describe('#Set SetAutopay', function () {
+//         this.timeout(10000)
 
-        it('setAutopay', async function () {
-            let res = await soter.set.setAutopay(false)
-            assert.equal(res.code, 0)
-        });
+//         it('setAutopay', async function () {
+//             let res = await soter.set.setAutopay(false)
+//             assert.equal(res.code, 0)
+//         });
 
-        it('setSignAutopayRawData', async function() {
-            let res = await soter.set.setSignAutopayRawData(false)
-            assert.isNotEmpty(res)    
-        })
+//         it('setSignAutopayRawData', async function() {
+//             let res = await soter.set.setSignAutopayRawData(false)
+//             assert.isNotEmpty(res)    
+//         })
 
-        it('setSignAutopay', async function() {
+//         it('setSignAutopay', async function() {
             
-            const newTronWeb = new TronWeb({
-                fullHost: 'https://api.trongrid.io',
-                privateKey: '07843e997dfe76f655274e14ea688ca1ba08550b93147fc0feca4ba7174868a7'
-            });
+//             const newTronWeb = new TronWeb({
+//                 fullHost: 'https://api.trongrid.io',
+//                 privateKey: '07843e997dfe76f655274e14ea688ca1ba08550b93147fc0feca4ba7174868a7'
+//             });
 
-            let rawData = await soter.set.setSignAutopayRawData(false)
+//             let rawData = await soter.set.setSignAutopayRawData(false)
 
-            let signature = await newTronWeb.trx.sign(newTronWeb.toHex(JSON.stringify(rawData)))
+//             let signature = await newTronWeb.trx.sign(newTronWeb.toHex(JSON.stringify(rawData)))
 
-            let res = await soter.set.setSignAutopay(
-                'THq4ALz1irs8XXckpQESAPcxFLFbRpGpCv', 
-                rawData, 
-                signature
-            )
+//             let res = await soter.set.setSignAutopay(
+//                 'THq4ALz1irs8XXckpQESAPcxFLFbRpGpCv', 
+//                 rawData, 
+//                 signature
+//             )
 
-            assert.equal(res.code, 0)
-        })
+//             assert.equal(res.code, 0)
+//         })
 
-    });
+//     });
 
-})
+// })
